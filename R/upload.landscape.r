c4bf0711a2ba5d866704012a76f6863a5dd78f39
Bruno Silva <bmsasilva@gmail.com>
2019-03-11 16:38:11 +0000

    first draft of upload.landscape function

--- R/upload.landscape.r
+++ R/upload.landscape.r
@@ -1,19 +1,27 @@
-#' Upload and process rasters or shapefiles to compute landscape metrics
-#' @title
-#' @description
-#' @param file blabla
-#' @param type blabla
-#' @usage upload.landscape (file, type)
-#' @return que valores a funcao retorna
-#' @examples exemplos de aplicacao com dados fornecidos pelo package
-#' @references artigos em que se baseia a funcao
+#' upload.landscape
+#' @title Dsn and layer name from .shp filepath
+#' @description Creates a valid dsn and layer name from a .shp filepath
+#' @param path The full path of the landscape file
+#' @param vec Boolean. TRUE if file is a shapefile, FALSE
+#' if is a raster.
+#' @usage upload.landscape(path, vec)
+#' @return A SpatialPolygonsDataFrame object is returned.
 #' @author Bruno Silva
 #' @export
-upload.landscape <- function (file, type) {
-#file
-#type - raster, vector
+upload.landscape <- function(path, vec = TRUE){
 
+  if(vec){
+  dsn <- strsplit(path, "/|[\\]")[[1]]
+  dsn <- paste(dsn[-c(length(dsn))], collapse = "/")
 
+  layer <- strsplit(path, "/|[\\]")[[1]]
+  layer <- layer[length(layer)]
+  layer <- strsplit(as.character(layer), "\\.")[[1]][1]
 
-
+ landscape <- rgdal::readOGR(dsn = dsn, layer = layer)
+  } else {
+    landscape <- raster(path)
+    landscape <- rasterToPolygons(landscape) # introduzir algumas opcoes desta conversao nos argumentos da funcao upload.landscape
+}
+ return(landscape)
 }
