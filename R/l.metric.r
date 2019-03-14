7b4fc58eb1b05014b3de555ce005e503231c2023
FMestre1 <mestre.frederico@gmail.com>
2019-03-14 22:30:20 +0000

    metrics update

--- R/l.metric.r
+++ R/l.metric.r
@@ -12,67 +12,147 @@
 #' @references artigos em que se baseia a funcao
 #' @author Frederico Mestre
 #' @export
-l.metric <- function (landscape, metric) {
-# landscape object produced by upload.landscape
-# metric - landscape metrics to be computed
-
-  if (class(rl)!="lconnect")#nao sei se lhe vais dar este nome 
+l.metric <- function (landscape,dist_min, metric) {
+  # landscape object produced by upload.landscape
+  # metric - landscape metrics to be computed
+  
+  if (class(rl)!="lconnect") 
   {
     stop(paste(rl, " should be an object of class class 'lconnect'.", sep=""), call. = FALSE)
   }
   
+  distance <- as.dist(landscape$distance)
+  
+  #create result vector
+  result <- c()
+  
+  if("NC" %in% metric)
+  {
+    grouping <- hclust(distance, "single")
+    clusters <- cutree(grouping, h=dist_min)
+    df0 <- data.frame(landscape$area_c, clusters)
+    result <- c(result,NC = max(clusters))
+  }
+  
+  if("LNK" %in% metric)
+  {
+    result <- c(result, LNK = sum(distance<500))
+  }
+  
+  if("SLC" %in% metric)
+  {
+    grouping <- hclust(distance, "single")
+    clusters <- cutree(grouping, h=dist_min)
+    df0 <- data.frame(landscape$area_c, clusters)
+    NC <- max(clusters)
+    Ac <- as.numeric(sum(landscape$area_c))
+    r0 <- rep(NA, NC)
+    for(i in 1:NC)
+    {
+      df1 <- df0[df0$clusters==i, ]
+      ci <- sum(df1[,1])
+      r0[i] <- ci
+    }
+    result <- c(result, SLC = max(r0))
+  }
+  
+  if("MSC" %in% metric)
+  {
+    grouping <- hclust(distance, "single")
+    clusters <- cutree(grouping, h=dist_min)
+    df0 <- data.frame(landscape$area_c, clusters)
+    NC <- max(clusters)
+    Ac <- as.numeric(sum(landscape$area_c))
+    result <- c(result, MSC = Ac/NC)
+  }
+  
+  #CCP
+  if("CCP" %in% metric)
+  {
+    #number of components
+    
+    grouping <- hclust(distance, "single")
+    clusters <- cutree(grouping, h=dist_min)
+    df0 <- data.frame(landscape$area_c, clusters)
+    NC <- max(clusters)
+    Ac <- as.numeric(sum(landscape$area_c))
+    r0 <- rep(NA, NC)
+    for(i in 1:NC)
+    {
+      df1 <- df0[df0$clusters==i, ]
+      ci <- sum(df1[,1])
+      r0[i] <- (ci/Ac)^2
+    }
+    result <- c(result, CCP = sum (r0))
+    
+  }
+  result <- c(result, CCP = sum (r0))#has to be corrected
+  
+  #LCP
+  if("LCP" %in% metric)
+  {
+    grouping <- hclust(distance, "single")
+    clusters <- cutree(grouping, h=dist_min)
+    df0 <- data.frame(landscape$area_c, clusters)
+    NC <- max(clusters)
+    r0 <- rep(NA, NC)
+    for(i in 1:NC)
+    {
+      df1 <- df0[df0$clusters==i, ]
+      ci <- sum(df1[,1])
+      r0[i] <- (ci/landscape$area_l)^2
+    }
+    result <- c(result, LCP = sum (r0))
+  }
 
-#create result vector
-result <- c()
-
-#CCP
-    if("CCP" %in% metric)
-{
-}
-result <- c(result, CCP = sum (r0))#has to be corrected
-
-#LCP
-    if("LCP" %in% metric)
-{
-}
-result <- c(result, LCP = sum(r0_vec))#has to be corrected
-
-#CPL
-    if("CPL" %in% metric)
-{
-}
-result <- c(result, CPL = mean(m0,na.rm=TRUE))
-
-#ECS
-    if("ECS" %in% metric)
-{
-}
-result <- c(result, ECS = a_num/a)
-
-#AWF
-    if("AWF" %in% metric)
-{
-}
-result <- c(result, AWF = (sum(paths[, 3]*paths[, 4]*paths[, 5])))
-
-#IIC
-    if("IIC" %in% metric)
-{
-}
-result <- c(result, IIC = sum(paths[, 6])/Al2)
-
-#PC
-    if("PC" %in% metric)
-{
-}
-result <- c(result, PC = (PCnum/Al2^2))
-
-#ECA
-    if("ECA" %in% metric)
-{
-}
-result <- c(result, ECA = EC)
-
-#provide results
-	return(round(result,5))
+  #CPL
+  if("CPL" %in% metric)
+  {
+  }
+  result <- c(result, CPL = mean(m0,na.rm=TRUE))
+  
+  #ECS
+  if("ECS" %in% metric)
+  {
+    grouping <- hclust(distance, "single")
+    clusters <- cutree(grouping, h=dist_min)
+    df0 <- data.frame(landscape$area_c, clusters)
+    NC <- max(clusters)
+    Ac <- as.numeric(sum(landscape$area_c))
+    r0 <- rep(NA, NC)
+    for(i in 1:NC)
+    {
+      df1 <- df0[df0$clusters==i, ]
+      ci <- sum(df1[,1])
+      r0[i] <- ci^2
+    }
+  }
+  result <- c(result, ECS = sum(r0)/Ac)
+  
+  #AWF
+  if("AWF" %in% metric)
+  {
+  }
+  result <- c(result, AWF = (sum(paths[, 3]*paths[, 4]*paths[, 5])))
+  
+  #IIC
+  if("IIC" %in% metric)
+  {
+  }
+  result <- c(result, IIC = sum(paths[, 6])/Al2)
+  
+  #PC
+  if("PC" %in% metric)
+  {
+  }
+  result <- c(result, PC = (PCnum/Al2^2))
+  
+  #ECA
+  if("ECA" %in% metric)
+  {
+  }
+  result <- c(result, ECA = EC)
+  
+  #provide results
+  return(round(result,5))
 }
