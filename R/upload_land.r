a61f7d6c48072359fb4d8ebe06f07251011477a0
Bruno Silva <bmsasilva@gmail.com>
2019-03-19 14:48:38 +0000

    plot and upload_land updated

--- R/upload_land.r
+++ R/upload_land.r
@@ -9,26 +9,36 @@
 #' If NULL (default option) a convex hull will be created and used as boundary.
 #' @param habitat vector with habitat categories. The categories can be numeric
 #' or character.
-#' @usage upload_land(land_path, bound_path = NULL, habitat)
+#' @usage upload_land(land_path, bound_path = NULL, habitat, min_dist = NULL)
 #' @return A lconnect object is returned.
 #' @examples vec_path <- system.file("extdata/vector.shp", package = "lconnect")
-#' landscape <- upload_land(vec_path, bound_path = NULL, habitat = 1)
+#' landscape <- upload_land(vec_path, bound_path = NULL,
+#'  habitat = 1, min_dist = NULL)
 #' @author Bruno Silva
 #' @author Frederico Mestre
 #' @export
-upload_land <- function(land_path, bound_path = NULL, habitat){
-  landscape <- sf::st_read(vec_path)
+upload_land <- function(land_path, bound_path = NULL, habitat, min_dist = NULL){
+  landscape <- sf::st_read(land_path, quiet = T)
   landscape <- landscape[landscape[[2]] == habitat,]
   landscape <- sf::st_union(landscape)
   if(is.null(bound_path)){
     boundary <- sf::st_convex_hull(landscape)
-  } else {
-    boundary <- sf::st_read(bound_path)
+  } else{
+    boundary <- sf::st_read(bound_path, quiet = T)
   }
   area_l <- sf::st_area(boundary)
   landscape <- sf::st_cast(landscape, "POLYGON")
   area_c <- sf::st_area(landscape)
   distance <- sf::st_distance(landscape)
+  distance <- as.dist(distance)
+  if(is.null(min_dist)){
+    clusters <- rep(1, length(landscape)) 
+  } else{
+    groups <- hclust(distance, "single")
+    clusters <- cutree(groups, h = min_dist)
+  }
+  landscape <- suppressWarnings(st_sf(clusters = clusters, 
+                                      geometry = landscape))
   object <- list(landscape = landscape, distance = distance, 
                  boundary = boundary, area_c = area_c, area_l = area_l)
   class(object) <- "lconnect"
