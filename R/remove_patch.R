6f3caf55019dac5464820a6320aa6b27a7322420
Bruno Silva <bmsasilva@gmail.com>
2019-03-20 08:55:25 +0000

    update remove_patch

--- R/remove_patch.R
+++ R/remove_patch.R
@@ -1,8 +1,9 @@
 #' @keywords internal
 remove_patch <- function(object, patch)
 {
-  object$landscape <- object$landscape$geometry[-patch]
-  object$distance <- object$distance[-patch,-patch] # mudar. ja nao e uma matriz de distancias mas sim um objecto dist
-  object$area_c <- object$area_c[-patch]
+  object$landscape <- object$landscape$geometry[-c(patch)]
+  object$distance <- as.matrix(object$distance)[-c(patch),-c(patch)]
+  object$distance <- as.dist(object$distance)
+  object$area_c <- object$area_c[-c(patch)]
   return(object)
 }
